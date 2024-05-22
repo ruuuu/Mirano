1cb50149986598fc86450e9c64e20146a0f7315f
ruuuu <rufinka_91@mail.ru>
2024-05-22 16:49:50 +0300

    add comments

--- src/scripts/ProductCard.jsx
+++ src/scripts/ProductCard.jsx
@@ -3,7 +3,7 @@ import { API_URL } from "./API.js";
 import { cartStore } from "./store.js";
 
 
-
+// в vite.config.js прописали  jsxInject: "import jsx from '@/jsx.js'", так мы связали jsx.jsx с этим компонентом ProductCard 
  // как в реакт (компонент jsx)
 export const ProductCard = (product) => {
 
